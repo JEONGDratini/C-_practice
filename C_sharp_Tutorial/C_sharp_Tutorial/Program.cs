ea05b5b216685311368946469f6230221f4c3691
aadevours73 <pw050503@naver.com>
2022-07-01 13:15:36 +0900

    initiate

---
+++ C_sharp_Tutorial/C_sharp_Tutorial/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_sharp_Tutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        static void square(int a)
+        {
+
+        }
+    }
+
+    
+}
