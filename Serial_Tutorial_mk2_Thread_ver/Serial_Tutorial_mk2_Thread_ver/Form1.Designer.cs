287b7053f8c578246acf304f71156e3e659ab4d4
aadevours73 <pw050503@naver.com>
2022-07-05 13:56:21 +0900

    Serial_Tutorial_mk2 스레드 버전 생성

---
+++ Serial_Tutorial_mk2_Thread_ver/Serial_Tutorial_mk2_Thread_ver/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Serial_Tutorial_mk2_Thread_ver
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.terminate = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.set_Port = new System.Windows.Forms.ComboBox();
+            this.output_guide = new System.Windows.Forms.Label();
+            this.measurement_result = new System.Windows.Forms.DataGridView();
+            this.Stop = new System.Windows.Forms.Button();
+            this.Start = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.measurement_result)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 12);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "현재상태 : 연결안됨";
+            // 
+            // terminate
+            // 
+            this.terminate.Location = new System.Drawing.Point(13, 199);
+            this.terminate.Name = "terminate";
+            this.terminate.Size = new System.Drawing.Size(116, 26);
+            this.terminate.TabIndex = 18;
+            this.terminate.Text = "리셋";
+            this.terminate.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(82, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 12);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "포트 선택";
+            // 
+            // set_Port
+            // 
+            this.set_Port.AccessibleName = "";
+            this.set_Port.FormattingEnabled = true;
+            this.set_Port.Location = new System.Drawing.Point(13, 30);
+            this.set_Port.Name = "set_Port";
+            this.set_Port.Size = new System.Drawing.Size(126, 20);
+            this.set_Port.TabIndex = 16;
+            // 
+            // output_guide
+            // 
+            this.output_guide.AutoSize = true;
+            this.output_guide.Location = new System.Drawing.Point(180, 15);
+            this.output_guide.Name = "output_guide";
+            this.output_guide.Size = new System.Drawing.Size(85, 12);
+            this.output_guide.TabIndex = 15;
+            this.output_guide.Text = "저항 측정 결과";
+            // 
+            // measurement_result
+            // 
+            this.measurement_result.AllowUserToOrderColumns = true;
+            this.measurement_result.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.measurement_result.Location = new System.Drawing.Point(182, 30);
+            this.measurement_result.Name = "measurement_result";
+            this.measurement_result.RowTemplate.Height = 23;
+            this.measurement_result.Size = new System.Drawing.Size(254, 220);
+            this.measurement_result.TabIndex = 14;
+            // 
+            // Stop
+            // 
+            this.Stop.Location = new System.Drawing.Point(13, 167);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(116, 26);
+            this.Stop.TabIndex = 13;
+            this.Stop.Text = "정지";
+            this.Stop.UseVisualStyleBackColor = true;
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(13, 135);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(116, 26);
+            this.Start.TabIndex = 12;
+            this.Start.Text = "시작";
+            this.Start.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(449, 262);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.terminate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.set_Port);
+            this.Controls.Add(this.output_guide);
+            this.Controls.Add(this.measurement_result);
+            this.Controls.Add(this.Stop);
+            this.Controls.Add(this.Start);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.measurement_result)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button terminate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox set_Port;
+        private System.Windows.Forms.Label output_guide;
+        private System.Windows.Forms.DataGridView measurement_result;
+        private System.Windows.Forms.Button Stop;
+        private System.Windows.Forms.Button Start;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Serial_Tutorial_mk2_Thread_ver/Serial_Tutorial_mk2_Thread_ver/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace Serial_Tutorial_mk2_Thread_ver
+{
+    public partial class Form1 : Form
+    {
+        int count = 1;//DataGrid에 들어갈 번호값
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form_Load(object sender, EventArgs e)
+        {
+            set_Port.DataSource = SerialPort.GetPortNames();//포트이름 가져오기
+            measurement_result.Columns.Add("colNum", "번호");//datagrid에 컬럼 집어넣기
+            measurement_result.Columns.Add("colRes", "저항값");
+
+
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            if (set_Port.Enabled)//연결이 안돼있으면 연결하고 이미 연결 돼있으면 타이머만 다시 시작한다.
+            {
+                serialPort1.PortName = set_Port.Text;//포트 고른거 가져오기
+                serialPort1.BaudRate = 9600;//통신속도
+                serialPort1.DataBits = 8;//data bit
+                serialPort1.StopBits = StopBits.One;
+                serialPort1.Parity = Parity.None;
+
+                serialPort1.Open();//통신 열기
+
+                label1.Text = "현재상태 : 연결됨";
+                set_Port.Enabled = false;
+            }
+            timer1.Start();
+
+        }
+
+
+        private void DataReceived(object sender, SerialDataReceivedEventArgs e)//시리얼 Data받았을 때 실행
+        {
+            this.Invoke(new EventHandler(serialReceived));//this -> form이다. invoke를 사용해서 스레드 충돌이 일어나지 않도록 해준다.
+        }
+
+        private void serialReceived(object sender, EventArgs e)//DataReceived에서 invoke로 호출한다.
+        {
+            string ascii = "";
+            string result = "";
+
+            while (true)// \n개행문자가 감지되면 수신을 종료하도록 설정.
+            {
+                bool Is_End = false;//개행문자 여부 불린값
+                int get_Data = serialPort1.ReadByte();//시리얼 포트에 수신된 데이터를 읽는다.1개 바이트만 읽는다.
+                ascii = ascii + string.Format("{0:X2}", get_Data) + " ";//스플릿 할 수 있게 각 바이트마다 띄어쓰기로 구분한다.
+                Is_End = ascii.Contains("0A");//\n의 16진수 표현 검색
+                if (Is_End)//개행문자 있으면 break;
+                    break;
+            }
+
+            string[] hexValues = ascii.Split(' ');
+
+            foreach (String hex in hexValues)
+            {
+                if (hex.Length > 0)//확인해보니까 hexvalues 배열의 크기가 실제 존재하는 아스키코드 문자 갯수보다 1개 많아서 오류가 남 -> 해결용 조건문
+                {
+                    int value = Convert.ToInt32(hex, 16);//16진수 hex값을 10진수로 바꾼다. 
+
+                    char charValue = (char)value;//10진수값을 아스키코드 변환을 한다.
+                    result = result + charValue;
+                }
+            }
+
+            measurement_result.Rows.Add(count, result + " Ω");//데이터그리드에 값집어넣기
+            count++;
+        }
+
+        private void Stop_Click(object sender, EventArgs e)//정지버튼 눌렀을 때
+        {
+            timer1.Stop();
+        }
+
+        private async void timer1_Tick(object sender, EventArgs e)//타이머 지정시간 경과때 마다 실행
+        {
+            serialPort1.Write("MEAS:RES?\r\n");
+        }
+
+        private void terminate_Click(object sender, EventArgs e)//리셋버튼 눌렀을 때
+        {
+            serialPort1.Close();
+            timer1.Stop();
+            label1.Text = "현재상태 : 연결안됨";
+            set_Port.Enabled = true;
+            measurement_result.Rows.Clear();
+            count = 1;
+        }
+
+    }
+}
