3b8a19085f65f7447fdb2bd1c14c3c636112d530
aadevours73 <pw050503@naver.com>
2022-07-07 16:17:22 +0900

    익명(Anonymous)메소드 실습예제와 메모

---
+++ CSharp_Tutorial_Folder/Anonymous_Method_Tutorial/Anonymous_Method_Tutorial/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Anonymous_Method_Tutorial
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //메서드 명을 따로 지정한다.
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+
+            //그 자리에서 무명메서드를 지정한다.
+            this.button2.Click += delegate(object s, EventArgs e)//반환값과 매개변수 지정.
+            {//메서드 내용
+                MessageBox.Show("button2 Clicked");
+            };
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("button1 Clicked");
+        }
+    }
+}
