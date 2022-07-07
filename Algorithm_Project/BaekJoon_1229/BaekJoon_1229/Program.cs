26a03ac789a6777f93058eb4f85c9fd5f4dcf28c
aadevours73 <pw050503@naver.com>
2022-07-07 18:05:15 +0900

    백준 1229번 육각수 문제 생성

---
+++ Algorithm_Project/BaekJoon_1229/BaekJoon_1229/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//백준 1229번 육각수 티어 골드4
+ namespace BaekJoon_1229
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //입력받기
+            int N = int.Parse(Console.ReadLine());
+
+            int count = 0;
+            while (N != 0) {
+                int n_h = 1;
+                while (true) {//시그마 공식을 이용해서 비교한다. 세부로직은 내일^^
+                    
+                }     
+            }
+
+        }
+    }
+}
