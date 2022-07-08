67b195ac24daa5be52f68acbc9946c17c6002ee4
aadevours73 <pw050503@naver.com>
2022-07-08 18:02:38 +0900

    BaekJoon_1992문제 생성

---
+++ Algorithm_Project/BaekJoon_1992/BaekJoon_1992/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//백준 문제 1992번 티어 실버1 쿼드트리 문제
+namespace BaekJoon_1992
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int [,] Quad_Tree = new int [N,N];
+            for (int i = 0; i < N; i++) {
+                string raw = Console.ReadLine();
+
+            }
+        }
+    }
+}
